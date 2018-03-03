6937fd7dac9db7bf7584d3a42b346ef4c4c8c3a3
Naor <naory159@gmail.com>
2018-03-03 21:35:00 +0200

    Add top header

--- app/src/main/java/com/ny/myapplication/customviewtest/MainActivity.java
+++ app/src/main/java/com/ny/myapplication/customviewtest/MainActivity.java
@@ -1,15 +1,20 @@
 package com.ny.myapplication.customviewtest;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    TextView headerTitle = null;
 
     Button button = null;
     Button bookmarkSaveButton = null;
@@ -24,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            // If the Android version is lower than Jellybean, use this call to hide
+            // the status bar.
+            if (Build.VERSION.SDK_INT < 16) {
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            } else {
+                View decorView = getWindow().getDecorView();
+                // Hide the status bar.
+                int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+                decorView.setSystemUiVisibility(uiOptions);
+                // Remember that you should never show the action bar if the
+                // status bar is hidden, so hide that too if necessary.
+                ActionBar actionBar = getActionBar();
+                actionBar.hide();
+            }
+        } catch(Exception ex) {
+
+        }
+
+        headerTitle = findViewById(R.id.header_title);
+
         button = findViewById(R.id.button3);
 
         card = findViewById(R.id.card);
@@ -37,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         bookmarkSaveButton.setTypeface(font);
         bookmarkEditText.setTypeface(font);
         bookmarkTextView.setTypeface(font);
+        headerTitle.setTypeface(font);
+        button.setTypeface(font);
 
         card.SetSlideUpInterpolator(new AccelerateInterpolator());
         card.SetSlideDownInterpolator(new AccelerateInterpolator());
